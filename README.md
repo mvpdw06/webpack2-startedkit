484e001bfd0a8e7fd020a3dffd9d8f230dd8e50b
Ryan Hsu <mvpdw06@gmail.com>
2017-04-26 23:36:51 +0800

    add all files.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webpack 2 - StartedKit
+
+## Dependency
+
+1. webpack v2
+2. webpack devServer
+3. react
+4. react-dom
+5. react-hot-loader
+6. babel-cli
+7. babel-loader
+8. babel-preset-es2015
+9. babel-preset-react
+10. babel-preset-stage-0
+
+## Target
+
+- [x] Migrate webpack settings from v1 to v2.
+- [x] Build production mode script.
+- [ ] webpack UglifyJsPlugin adjust.
+- [ ] Babel setting adjust.
+- [ ] React-hot-loader setting adjust.
---
+++ webpack.config.babel.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const env = process.env.NODE_ENV || 'development';
+const isDevEnv = env === 'development';
+
+// webpack settings
+const getDevtoolSetting = () => {
+    return isDevEnv ?
+    'cheap-module-eval-source-map' :
+    'cheap-module-source-map';
+}
+const getPluginsSetting = () => {
+    const plugins = [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ];
+    if (isDevEnv) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin()
+        );
+    } else {
+        // plugins.push(
+        //     new webpack.optimize.UglifyJsPlugin({
+        //         compress: {
+        //             warnings: false,
+        //             drop_console: false,
+        //         }
+        //     })
+        // );
+    }
+    return plugins;
+}
+
+const webpackConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/]
+            }
+        ]
+    },
+    devtool: getDevtoolSetting(),
+    plugins: getPluginsSetting()
+}
+
+const AppConfig = Object.assign({}, webpackConfig, {
+    entry: [
+        'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		path.join(__dirname, 'src/index')
+    ],
+    output: {
+        path: path.join(__dirname, 'output/assets'),
+		filename: 'app.js',
+		publicPath: '/assets'
+    }
+});
+
+module.exports = AppConfig;
